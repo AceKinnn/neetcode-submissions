260a44486b0b279c17a63ec6449dca80cf9b2514
AceKinnn <89128620+AceKinnn@users.noreply.github.com>
2026-08-31 14:34:37 +0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        frequency_dict = dict(Counter(nums))
+        
+        for count in frequency_dict.values():
+            if count > 1:
+                return True
+        return False
+    
+print(Solution().hasDuplicate([1,2,2,3,4]))
